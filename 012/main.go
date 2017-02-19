c6f30ae7ff1b0c2a235049af6fd15f6f4c948f43
CatalinaRose <gatajuegos@gmail.com>
2017-02-18 21:05:25 -0800

    templatedatapassing

--- 012/main.go
+++ 012/main.go
@@ -1,10 +1,23 @@
 package main
 
-import ("no/andthen"
+import (
 	"net/http"
+	"io"
+	"fmt"
 )
 
 
+type person int
+
+func(p person)ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	io.WriteString(w, "Hello FReaks, Freaks do it better")
+}
+
 func main() {
-	http.ListenAndServe(":8080", nil)
+	var weed person
+	http.Handle("/", weed)
+	err:= http.ListenAndServe(":8081", nil)
+	if err != nil {
+		fmt.Println(err)
+	}
 }
---
+++ 013/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"net/http"
+	"html/template"
+
+)
+
+var tpl *template.Template
+
+func init() {
+	tpl = template.Must(template.ParseGlob("templates/*.gohtml"))
+}
+
+func main() {
+	http.HandleFunc("/", index)
+	http.HandleFunc("/about", about)
+	http.Handle("/favicon.ico", http.NotFoundHandler())
+	http.ListenAndServe(":8080", nil)
+}
+
+func index(w http.ResponseWriter, r *http.Request) {
+	tpl.ExecuteTemplate(w, "index.gohtml", "Im am not of this world")
+}
+
+func about(w http.ResponseWriter, r *http.Request) {
+	tpl.ExecuteTemplate(w, "about.gohtml", 420)
+}
