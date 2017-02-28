b43709bbb8713a708b65a69754c16734bf6e19c2
CatalinaRose <gatajuegos@gmail.com>
2017-02-28 13:13:11 -0800

    mapstructtemplates

---
+++ 014/maingo2.go
@@ -0,0 +1,28 @@
+package main
+
+
+import (
+	"net/http"
+	"text/template"
+)
+
+var tpl *template.Template
+
+func init() {
+	tpl = template.Must(template.ParseGlob("templates/*.gohtml"))
+}
+
+func main() {
+	http.HandleFunc("/", index)
+	http.ListenAndServe(":8085", nil)
+}
+
+func index(w http.ResponseWriter, r *http.Request) {
+	m := map[string]bool{}
+	m["Pot"] = true
+	m["Babe"] = true
+	m["Gata"] = true
+	tpl.ExecuteTemplate(w, "index.gohtml", m)
+
+}
+
--- 015/main.go
+++ 015/main.go
@@ -6,16 +6,16 @@ import (
 )
 
 func main() {
-	http.HandleFunc("/", puppy)
-	http.Handlefunc("/harley.jpg", dogPic)
+	http.HandleFunc("/", TechNine)
+	http.HandleFunc("/technine.jpg", TechNinePic)
 	http.ListenAndServe(":8084", nil)
 }
 
-func dog(w http.ResponseWriter, req *http.Request) {
+func TechNine(w http.ResponseWriter, req *http.Request) {
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	io.WriteString(w, '<img src="toby.jpg"> ')
+	io.WriteString(w, `<img src="technine.jpg"> `)
 }
 
-func dogPic(w http.ResponseWriter, rep *http.Request) {
-	http.ServeFile(w, req, "public/img/toby.jpg")
+func TechNinePic(w http.ResponseWriter, req *http.Request) {
+	http.ServeFile(w, req, "img/technine.jpg")
 }
